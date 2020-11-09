8ea677bd5f025f72368941c44129e62d7ae40219
mounvip1525 <mounvip1525@gmail.com>
2020-11-09 16:44:17 +0530

    CSS animation added

--- script.js
+++ script.js
@@ -6,15 +6,30 @@ const nav3=document.getElementById('nav-3');
 const nav4=document.getElementById('nav-4');
 const nav5=document.getElementById('nav-5');
 
+const navItems=[nav1,nav2,nav3,nav4,nav5];
+
+//Control Navigation Animation
+function navAnimation(flowdirection1,flowdirection2){
+    navItems.forEach(item,index){
+        item.classList.replace(`slide-${flowdirection1}-${index+1}`,`slide-${flowdirection2}-${index+1}`)
+    }
+}
 
 function toggleNav(){
     menuBars.classList.toggle('change');
+    overlay.classList.toggle('active-menuBar');
+    if(overlay.classList.contains('active-menuBar')){
+        overlay.classList.replace('overlay-slide-out','overlay-slide-in');
+         navAnimation('in','out');
+    }
+    else{
+        overlay.classList.replace('overlay-slide-in','overlay-slide-out');
+        navAnimation('out','in');
+    }
 }
 menuBars.addEventListener('click',toggleNav);
-nav1.addEventListener('click',toggleNav);
-nav2.addEventListener('click',toggleNav);
-nav3.addEventListener('click',toggleNav);
-nav4.addEventListener('click',toggleNav);
-nav5.addEventListener('click',toggleNav);
+navItems.forEach(item=>{
+    itemm.addEventListener('click',toggleNav);
+});
 
 
